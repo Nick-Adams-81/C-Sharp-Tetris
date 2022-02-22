0a0bcd1fb24b911d715d0153ce5ab2816d5fc34c
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-22 10:22:49 -0600

    added S Block

--- Tetris/OBlock.cs
+++ Tetris/OBlock.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 namespace Tetris
 {
-    public class OBlock
+    public class OBlock : Block
     {
-        public OBlock()
+
+        private readonly Position[][] tiles = new Position[][]
         {
-        }
+            new Position[] { new(0,0), new(0,1), new(1,0), new(1,1)}
+        };
+
+        public override int Id => 4;
+
+        protected override Position StartOffset =>  new Position(0, 4);
+
+        protected override Position[][] Tiles => tiles;
     }
+
 }
